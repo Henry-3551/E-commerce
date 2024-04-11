05cbf0fe7c7a9451c7747f12ab4abcff34383189
Chukwuemerie Edmumd <104362482+Henry-3551@users.noreply.github.com>
2024-04-11 08:17:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
+const body = document.querySelector('#body');
 
+const thirdModal = document.querySelector('.card-3');
+
+const homeModal = document.querySelector('.entice-modal');
+    
+const setBg = document.querySelector('.settings-modal');
+
+     
+ if (sessionStorage.getItem("userPhone") === null) {
+
+  function prompt(){
+    thirdModal.style.display = 'block';
+    body.classList.add("active");
+  }
+  
+   function redirectUser(){
+     thirdModal.style.display = 'none';
+     body.classList.remove("active");
+     window.location.replace("login.html")
+   }
+   
+   let a = setTimeout(prompt, 1000);
+   
+   let b = setTimeout(redirectUser, 2500);
+ };
+ 
+ function showSet(){
+   
+   setBg.style.display = 'block';
+ }
+ 
+ /*window.onscroll = function() {
+   displayEntice();
+ }
+ 
+ function displayEntice() {
+   
+ 
+   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+     homeModal.classList.add("active");
+   } else {
+     homeModal.classList.remove("active");
+   }
+ }*/
